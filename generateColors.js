1b5464fa012a6ec27622c72be5365091da28cba6
lezan <zanchileonardo@gmail.com>
2020-05-30 18:22:05 +0200

    Generate new color

--- generateColors.js
+++ generateColors.js
@@ -143,10 +143,45 @@ var generateRandomColors=function(number){
 	});
 }
 
-const colors1 = generateRandomColors(30);
+function hexToRgb(hex) {
+	const m = hex.match(/^#?([\da-f]{2})([\da-f]{2})([\da-f]{2})$/i);
+
+    return `[${parseInt(m[1], 16)}, ${parseInt(m[2], 16)}, ${parseInt(m[3], 16)}]`;
+}
+
+function hslToRgb(hsl) {
+	const h = Number(hsl.split(',')[0].replace('hsl(', ''));
+	const s = Number(hsl.split(',')[1].replace('%', '')) / 100;
+	const l = Number(hsl.split(',')[2].replace('%)', '')) / 100;
+
+    let r, g, b;
+
+    if (s == 0) {
+        r = g = b = l; // achromatic
+    } else {
+        const hue2rgb = function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1/6) return p + (q - p) * 6 * t;
+            if (t < 1/2) return q;
+            if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return `[${Math.round(r * 255)}, ${Math.round(g * 255)}, ${Math.round(b * 255)}]`;
+}
+
+const colors1 = generateRandomColors(10);
 let txtColors1 = '';
 colors1.forEach((d) => {
-	txtColors1 += `${d}\n`;
+	txtColors1 += `${hexToRgb(d)}\n`;
 });
 
 writeFileSync('./colors1.txt', txtColors1);
@@ -157,13 +192,13 @@ function selectColor(number) {
 }
 
 const colors2 = [];
-for (let i = 0; i < 30; i += 1) {
+for (let i = 0; i < 10; i += 1) {
 	colors2.push(selectColor(Math.random()));
 }
 
 let txtColors2 = '';
 colors2.forEach((d) => {
-	txtColors2 += `${d}\n`;
+	txtColors2 += `${hslToRgb(d)}\n`;
 });
 
 writeFileSync('./colors2.txt', txtColors2);
